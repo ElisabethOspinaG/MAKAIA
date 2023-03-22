81cfcd79b23a9977a3b52a9fcb929dd397894710
elisabeth ospina <elisabethospinag@gmail.com>
2023-03-22 01:56:30 -0500

    pagina yo tube

--- EJERCICIO YOU TUBE/scripts/data.js
+++ EJERCICIO YOU TUBE/scripts/data.js
@@ -42,7 +42,7 @@ export const listVideos = [
     {
         id: 4,
         name: "Figma tutorial - Cómo compartir tus diseños con desarrollo",
-        image: "./img/img4.png",
+        image: "../img/img4.png",
         video: "https://www.youtube.com/embed/igpvckbxhJs",
         author: "uxcristopher",
         imgAuthor: "./img/avatar4.png",
@@ -55,7 +55,7 @@ export const listVideos = [
     {
         id: 5,
         name: "Tipos de programadores",
-        image: "./img/img5.png",
+        image: "../img/img5.png",
         video: "https://www.youtube.com/embed/KGj-5E3EG0Q",
         author: "Leonidas Esteban",
         imgAuthor: "./img/avatar5.png",
@@ -67,7 +67,7 @@ export const listVideos = [
     {
         id: 6,
         name: "Cómo diseñar correos. ¡PASO A PASO!",
-        image: "./img/img6.png",
+        image: "../img/img6.png",
         video: "https://www.youtube.com/embed/It407UNmYoY",
         author: "uxcristopher",
         imgAuthor: "./img/avatar6.png",
@@ -79,7 +79,7 @@ export const listVideos = [
     {
         id: 7,
         name: "Crea tu portafolio y certifícate con /Proyectos",
-        image: "./img/img7.png",
+        image: "../img/img7.png",
         video: "https://www.youtube.com/embed/10Blk7gYQ38",
         author: "Leonidas Esteban",
         imgAuthor: "./img/avatar7.png",
@@ -91,7 +91,7 @@ export const listVideos = [
     {
         id: 8,
         name: "Paletas de color para tus proyectos usando Figma",
-        image: "./img/img8.png",
+        image: "../img/img8.png",
         video: "https://www.youtube.com/embed/g3CbDto2zzs",
         author: "Leonidas Esteban",
         imgAuthor: "./img/avatar8.png",
@@ -103,7 +103,7 @@ export const listVideos = [
     {
         id: 9,
         name: "EURODANCE 90'S MIX",
-        image: "./img/img9.png",
+        image: "../img/img9.png",
         video: "https://www.youtube.com/embed/7QqKcfOiBHU",
         author: "Leonidas Esteban",
         imgAuthor: "./img/avatar9.png",
@@ -115,7 +115,7 @@ export const listVideos = [
     {
         id: 10,
         name: "Vintage Culture Epic DJ Set From Ibirapuera, Brazil",
-        image: "./img/img10.png",
+        image: "../img/img10.png",
         video: "https://www.youtube.com/embed/RrH7XtKeq0M",
         author: "DJ Vintage",
         imgAuthor: "./img/avatar10.png",
@@ -127,7 +127,7 @@ export const listVideos = [
     {
         id: 11,
         name: "Above & Beyond Live At Ultra Music Festival Miami 2018",
-        image: "./img/img11.png",
+        image: "../img/img11.png",
         video: "https://www.youtube.com/embed/RrH7XtKeq0M",
         author: "Above & beyond",
         imgAuthor: "./img/avatar11.png",
@@ -139,7 +139,7 @@ export const listVideos = [
     {
         id: 12,
         name: "Purple Disco Machine, Special Mix 2021. Best Songs & Remixes",
-        image: "./img/img12.png",
+        image: "../img/img12.png",
         video: "https://www.youtube.com/embed/LyCEN_zTE54",
         author: "Caroune",
         imgAuthor: "./img/avatar12.png",
